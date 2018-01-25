5443483f4cfd62f881792d6f208349a6c12f9841
lanickerson6392 <lanickerson6392@L438R2S8.STUDENT.ad.fgcu.edu>
2018-01-25 18:38:01 -0500

    First commit

---
+++ LynsiNickersonIntegration/src/Main.java
@@ -0,0 +1,11 @@
+//Lynsi Nickerson
+//My project that integrates all of the java skills I have learned in my COP 2006 class.
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+        // sysout ctrl+space
+	}
+
+}
